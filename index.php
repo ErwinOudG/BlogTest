faea3de19f90654626014484a2bd3fee475c898f
RyanvanderLugt <vanderlugt.ryan@gmail.com>
2017-12-19 14:12:21 +0100

    Goed

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $obj3->setProperty("Ja wat nou ?!?");
 	<link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="css/style.css">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script type="text/javascript" src="js/materialize.min.js"></script>
 	<link href="css/prism.css" rel="stylesheet" />
